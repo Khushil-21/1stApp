36b49a7dd8c0e0ea1dc14398260fc3ed6120fa1c
Khushil-21 <khushilshah2105@gmail.com>
2025-02-12 07:43:44 +0530

    added nav btns

--- app/src/main/java/com/royal/firstapp/ContactActivity.java
+++ app/src/main/java/com/royal/firstapp/ContactActivity.java
@@ -1,14 +1,22 @@
 package com.royal.firstapp;
 
 import android.os.Bundle;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ContactActivity extends AppCompatActivity {
+
+    ImageButton imgBtnCall;
+    ImageButton imgBtnChat;
+    ImageButton imgBtnPerson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,49 @@ public class ContactActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        imgBtnCall = findViewById(R.id.imgBtnCall);
+        imgBtnChat  = findViewById(R.id.imgBtnChat);
+        imgBtnPerson = findViewById(R.id.imgBtnPerson);
+
+        Drawable chatTwoTone = ContextCompat.getDrawable(getApplicationContext(),R.drawable.chat_twotone_32);
+        Drawable chatOutline = ContextCompat.getDrawable(getApplicationContext(),R.drawable.chat_outline_32);
+
+        Drawable callTwoTone = ContextCompat.getDrawable(getApplicationContext(),R.drawable.call_twotone_32);
+        Drawable callOutline  = ContextCompat.getDrawable(getApplicationContext(),R.drawable.call_outline_32);
+
+        Drawable personTwoTone = ContextCompat.getDrawable(getApplicationContext(),R.drawable.person_twotone_32);
+        Drawable personOutline  = ContextCompat.getDrawable(getApplicationContext(),R.drawable.person_outline_32);
+
+
+        imgBtnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgBtnCall.setImageDrawable(callOutline);
+                imgBtnChat.setImageDrawable(chatTwoTone);
+                imgBtnPerson.setImageDrawable(personOutline);
+            }
+        });
+
+        imgBtnPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgBtnCall.setImageDrawable(callOutline);
+                imgBtnChat.setImageDrawable(chatOutline);
+                imgBtnPerson.setImageDrawable(personTwoTone);
+            }
+        });
+
+        imgBtnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imgBtnCall.setImageDrawable(callTwoTone);
+                imgBtnChat.setImageDrawable(chatOutline);
+                imgBtnPerson.setImageDrawable(personOutline);
+            }
+        });
+
+
+
     }
 }
