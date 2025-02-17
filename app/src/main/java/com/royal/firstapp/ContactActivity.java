c1a3a76f7b771c2a0e5466fc8ecc7a93bae152e6
Khushil-21 <khushilshah2105@gmail.com>
2025-02-17 15:09:03 +0530

    On Nav CLick Calling Fragments

--- app/src/main/java/com/royal/firstapp/ContactActivity.java
+++ app/src/main/java/com/royal/firstapp/ContactActivity.java
@@ -11,6 +11,12 @@ import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.royal.firstapp.fragments.CallFragment;
+import com.royal.firstapp.fragments.ChatFragment;
+import com.royal.firstapp.fragments.PersonFragment;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -21,6 +27,7 @@ public class ContactActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this); // this is the reason of overlapping of the layout on top
         setContentView(R.layout.activity_contact);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -41,6 +48,15 @@ public class ContactActivity extends AppCompatActivity {
         Drawable personTwoTone = ContextCompat.getDrawable(getApplicationContext(),R.drawable.person_twotone_32);
         Drawable personOutline  = ContextCompat.getDrawable(getApplicationContext(),R.drawable.person_outline_32);
 
+        imgBtnChat.setImageDrawable(chatTwoTone);
+        imgBtnCall.setImageDrawable(callOutline);
+        imgBtnPerson.setImageDrawable(personOutline);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameMasterContact,new ChatFragment());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
 
         imgBtnChat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +64,12 @@ public class ContactActivity extends AppCompatActivity {
                 imgBtnCall.setImageDrawable(callOutline);
                 imgBtnChat.setImageDrawable(chatTwoTone);
                 imgBtnPerson.setImageDrawable(personOutline);
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frameMasterContact,new ChatFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
 
@@ -57,6 +79,12 @@ public class ContactActivity extends AppCompatActivity {
                 imgBtnCall.setImageDrawable(callOutline);
                 imgBtnChat.setImageDrawable(chatOutline);
                 imgBtnPerson.setImageDrawable(personTwoTone);
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frameMasterContact,new PersonFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
 
@@ -66,6 +94,12 @@ public class ContactActivity extends AppCompatActivity {
                 imgBtnCall.setImageDrawable(callTwoTone);
                 imgBtnChat.setImageDrawable(chatOutline);
                 imgBtnPerson.setImageDrawable(personOutline);
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frameMasterContact,new CallFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
 
